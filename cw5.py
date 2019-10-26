34fec3a9e22cd23471d9753749e4fcdc79d85e04
AnnaAwaria <retrorain@gmail.com>
2019-10-26 14:15:38 +0200

    Python5

---
+++ cw5.py
@@ -0,0 +1,9 @@
+tekst = input("Podaj liczbę: ")
+liczba = int(tekst)
+
+if liczba % 2 == 0:
+    print("Parzysta")
+else:
+    print("Nieparzysta")
+
+print("Dobranoc!")
---
+++ palindrome.py
@@ -0,0 +1,11 @@
+x = str(input("Podaj słowo: "))
+y = ""
+
+for i in x:
+    y = i + y
+if (x==y):
+        print("TAK")
+else:
+        print("NIE")
+
+
